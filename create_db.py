2159f2478a981a365349708698b1bae1aefb9e42
Ricsoon <painonex2@gmail.com>
2022-07-25 14:04:38 -0300

    Criando banco de dados e instanciando hoteis utilizando sqlite3

---
+++ create_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE IF NOT EXISTS hotels (hotel_id text PRIMARY KEY, name text, rank real, daily real, city text)"
+
+create_hotel = "INSERT INTO hotels VALUES ('alpha', 'Alpha Hotel', 4.3, 345.30, 'Rio de Janeiro')"
+
+cursor.execute(create_table)
+cursor.execute(create_hotel)
+
+connection.commit()
+connection.close()
