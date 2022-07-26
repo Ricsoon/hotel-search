1b7b236e24a76ecf0794e5695fdab75be4090987
Ricsoon <painonex2@gmail.com>
2022-07-26 13:59:09 -0300

    Adicionando regras de login para determinadas funções

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 class Hotels(Resource):
     def get(self):
@@ -19,6 +20,7 @@ class Hotel(Resource):
             return hotel.json()
         return {'message': 'Hotel not found.'}, 404 #Not found alert
 
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 #Bad Request
@@ -32,7 +34,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurrred trying to save hotel.'}, 500 #Internal Server Error
         return hotel.json()
         
-
+    @jwt_required()
     def put(self, hotel_id):
 
         data = Hotel.arguments.parse_args()
@@ -48,6 +50,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurrred trying to save hotel.'}, 500 #Internal Server Error
         return hotel.json(), 201 #Created alert
 
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
