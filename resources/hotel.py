a96e4d588aa50bc0eeb5308d8b5bec5562161362
Ricsoon <painonex2@gmail.com>
2022-07-26 19:38:02 -0300

    Adicionando filtros para a busca de hoteis por nome, rank e valor

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,10 +1,66 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+def normalize_path_params(city=None, rank_min=0,rank_max=5, daily_min=0, daily_max=10000, limit=50, offset=0, **data):
+    if city:
+        return {
+            "rank_min": rank_min,
+            "rank_max": rank_max,
+            "daily_min": daily_min,
+            "daily_max": daily_max,
+            "city": city,
+            "limit": limit,
+            "offset": offset
+        }
+    return {
+            "rank_min": rank_min,
+            "rank_max": rank_max,
+            "daily_min": daily_min,
+            "daily_max": daily_max,
+            "limit": limit,
+            "offset": offset
+        }
+
+# Path example -> /hotels?city=Recife&rank_min=4&daily_max=400
+path_params = reqparse.RequestParser()
+path_params.add_argument('city', type=str)
+path_params.add_argument('rank_min', type=float)
+path_params.add_argument('rank_max', type=float)
+path_params.add_argument('daily_min', type=float)
+path_params.add_argument('daily_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 class Hotels(Resource):
     def get(self):
-        return {'hotels': [hotel.json() for hotel in HotelModel.query.all()]}
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+
+        data = path_params.parse_args()
+        valid_data = {key: data[key] for key in data if data[key] is not None}
+        parameters = normalize_path_params(**valid_data,)
+
+        if not parameters.get('city'):
+            query = "SELECT * FROM hotels WHERE (rank >= ? and rank <= ?) and (daily >= ? and daily <= ?) LIMIT ? OFFSET ?"
+            tupla = tuple([parameters[key] for key in parameters])
+            result = cursor.execute(query, tupla)
+        else:
+            query = "SELECT * FROM hotels WHERE (rank >= ? and rank <= ?) and (daily >= ? and daily <= ?) and city = ? LIMIT ? OFFSET ?"
+            tupla = tuple([parameters[key] for key in parameters])
+            result = cursor.execute(query, tupla)
+        
+        hotels = []
+        for row in result:
+            hotels.append({
+            'hotel_id': row[0],
+            'name': row[1],
+            'rank': row[2],
+            'daily': row[3],
+            'city': row[4]
+            })
+        return {'hotels': [hotels]}
 
 class Hotel(Resource):
 
